73a2f83602261ca98f6d0d3d0b4b0565329558e1
netniti000 <netniti000@yahoo.co.jp>
2020-06-02 23:06:53 +0900

    'paginationの実装'

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,16 +1,24 @@
  class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
    def index
-    @boards = Board.all
+    @boards = Board.page(params[:page])
    end
 
    def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
    end
 
    def create
-    board = Board.create(board_params)
-    redirect_to board
+    board = Board.new(board_params)
+      if board.save
+         flash[:notice] = "『#{board.title}』の掲示板を作成しました"
+         redirect_to board
+      else
+         redirect_to new_board_path, flash: {
+            board: board,
+            error_messages: board.errors.full_messages
+      }
+      end
    end
 
    def show
@@ -29,8 +37,7 @@
 
    def destroy
     @board.delete
-
-    redirect_to boards_path
+    redirect_to boards_path, flash: { notice: "『#{@board.title}』の掲示板が削除されました。"}
    end
    
    private
